2205900110b263b01176bb69e185d9bd99022cce
Mada467 <madalinalexandru467@gmail.com>
2026-01-04 18:58:50 +0200

    Mici retusuri,zoom de schimbat sus

--- Patrat.c
+++ Patrat.c
@@ -12,22 +12,18 @@
 #define M_PI 3.14
 #endif
 
-// --- PROTOTIPURI FUNCTII ---
-void myinit(void);
-void CALLBACK display(void);
-void CALLBACK myReshape(GLsizei w, GLsizei h);
-void CALLBACK MutaStanga(void);
-void CALLBACK MutaDreapta(void);
-void CALLBACK MutaSus(void);
-void CALLBACK MutaJos(void);
 
-// --- VARIABILE GLOBALE ---
 static GLfloat x = 0.0;
 static GLfloat y = 0.0;
 static GLfloat z = -300.0;
-GLuint texturi[3];  // 0=maner, 1=ramă, 2=imagine fundal
 
-// --- INCARCARE BMP ---
+
+static GLfloat zoomFactor = 3.0f;       
+static GLfloat factorConversie = 3.38f;
+
+GLuint texturi[3]; // 0=maner, 1=rama, 2=imagine fundal
+
+// --- INCARCARE TEXTURI ---
 void IncarcaTextura(const char* caleFisier, int indexTextura) {
     BITMAP bmp;
     HBITMAP hBmp = (HBITMAP)LoadImageA(NULL, caleFisier, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
@@ -36,10 +32,14 @@ void IncarcaTextura(const char* caleFisier, int indexTextura) {
         GetObject(hBmp, sizeof(bmp), &bmp);
         glGenTextures(1, &texturi[indexTextura]);
         glBindTexture(GL_TEXTURE_2D, texturi[indexTextura]);
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+        // REGLAJ MARGINI: Folosim REPEAT pentru a elimina liniile de taiere
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, bmp.bmWidth, bmp.bmHeight,
             0, GL_BGR_EXT, GL_UNSIGNED_BYTE, bmp.bmBits);
         DeleteObject(hBmp);
@@ -50,7 +50,6 @@ void myinit(void) {
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glEnable(GL_DEPTH_TEST);
 
-    // Încărcăm toate texturile (BMP)
     IncarcaTextura("Lemn.bmp", 0);
     IncarcaTextura("Lemn.bmp", 1);
     IncarcaTextura("Saturno.bmp", 2);
@@ -59,133 +58,97 @@ void myinit(void) {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-// Funcție pentru desenare cerc texturat (pentru zoom)
-void DeseneazaCercTexturat(float centerX, float centerY, float radius, float texCenterX, float texCenterY, float texRadius) {
-    int segments = 50;
-
-    glBegin(GL_TRIANGLE_FAN);
-
-    // Centrul cercului
-    glTexCoord2f(texCenterX, texCenterY);
-    glVertex2f(centerX, centerY);
-
-    // Punctele pe circumferință
-    for (int i = 0; i <= segments; i++) {
-        float angle = 2.0f * M_PI * i / segments;
-        float cx = centerX + radius * cos(angle);
-        float cy = centerY + radius * sin(angle);
-
-        float tx = texCenterX + texRadius * cos(angle);
-        float ty = texCenterY + texRadius * sin(angle);
-
-        glTexCoord2f(tx, ty);
-        glVertex2f(cx, cy);
-    }
-
-    glEnd();
-}
-
-// Funcție pentru desenare textură fundal
-void DeseneazaFundalTextura(float zoomFactor, float centerX, float centerY) {
+void DeseneazaFundalSimplu() {
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, texturi[2]);
     glColor3f(1.0f, 1.0f, 1.0f);
 
-    if (zoomFactor == 1.0f) {
-        // Fundal normal - întreaga imagine
-        glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f); glVertex2f(0.0f, 0.0f);
-        glTexCoord2f(1.0f, 0.0f); glVertex2f(800.0f, 0.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex2f(800.0f, 600.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex2f(0.0f, 600.0f);
-        glEnd();
-    }
-    else {
-        // Zoom - desenăm CERC exact cât lentila
-        float lupaRadius = 90.0f;
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f); glVertex2f(0.0f, 0.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex2f(800.0f, 0.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex2f(800.0f, 600.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex2f(0.0f, 600.0f);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+}
 
-        float texCenterX = centerX / 800.0f;
-        float texCenterY = centerY / 600.0f;
-        float texRadiusX = (lupaRadius / zoomFactor) / 800.0f;
-        float texRadiusY = (lupaRadius / zoomFactor) / 600.0f;
+void DeseneazaFundalPentruZoom() {
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texturi[2]);
+    glColor3f(1.0f, 1.0f, 1.0f);
 
-        // Calculăm media pentru aspect ratio corect
-        float texRadius = (texRadiusX + texRadiusY) / 2.0f;
-
-        // Desenăm cercul texturat
-        DeseneazaCercTexturat(centerX, centerY, lupaRadius,
-            texCenterX, texCenterY, texRadius);
-    }
-
+    glBegin(GL_QUADS);
+    // Coordonate extinse pentru a acoperi marginile in timpul zoom-ului
+    glTexCoord2f(-1.0f, -1.0f); glVertex2f(-800.0f, -600.0f);
+    glTexCoord2f(2.0f, -1.0f); glVertex2f(1600.0f, -600.0f);
+    glTexCoord2f(2.0f, 2.0f); glVertex2f(1600.0f, 1200.0f);
+    glTexCoord2f(-1.0f, 2.0f); glVertex2f(-800.0f, 1200.0f);
+    glEnd();
     glDisable(GL_TEXTURE_2D);
 }
 
 void CALLBACK display(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glLoadIdentity();
 
-    // --- PASUL 1: DESENARE FUNDAL NORMAL ---
-    glDisable(GL_STENCIL_TEST);
+    // --- PASUL 1: FUNDAL BAZA ---
     glDisable(GL_DEPTH_TEST);
-
     glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    gluOrtho2D(0, 800, 0, 600);
+    glPushMatrix(); glLoadIdentity(); gluOrtho2D(0, 800, 0, 600);
     glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-
-    DeseneazaFundalTextura(1.0f, 0.0f, 0.0f);
-
+    glPushMatrix(); glLoadIdentity();
+    DeseneazaFundalSimplu();
     glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_PROJECTION); glPopMatrix();
 
-    // --- PASUL 2: CALCULARE POZITIE LUPA ---
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+    // --- PASUL 2: MASCA STENCIL ---
+    glMatrixMode(GL_PROJECTION); glLoadIdentity();
     gluPerspective(45.0, 800.0 / 600.0, 1.0, 1000.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+
+    glEnable(GL_STENCIL_TEST);
+    glStencilFunc(GL_ALWAYS, 1, 0xFF);
+    glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
+    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+
+    glPushMatrix();
     glTranslatef(x, y, z);
+    GLUquadricObj* masca = gluNewQuadric();
+    gluDisk(masca, 0, 35, 45, 1);
+    gluDeleteQuadric(masca);
+    glPopMatrix();
 
-    GLdouble modelMatrix[16], projMatrix[16];
-    GLint viewport[4];
-    glGetDoublev(GL_MODELVIEW_MATRIX, modelMatrix);
-    glGetDoublev(GL_PROJECTION_MATRIX, projMatrix);
-    glGetIntegerv(GL_VIEWPORT, viewport);
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
 
-    GLdouble screenX, screenY, screenZ;
-    gluProject(0, 0, 0, modelMatrix, projMatrix, viewport, &screenX, &screenY, &screenZ);
+    // --- PASUL 3: ZOOM 
+    glStencilFunc(GL_EQUAL, 1, 0xFF);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
 
-    // Calculăm zoom fix (fără variație pe Z)
-    float zoomFactor = 2.0f;  // Zoom fix de 2x
-
-    // --- PASUL 3: DESENARE ZOOM (CERC PERFECT) ---
     glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    gluOrtho2D(0, 800, 0, 600);
+    glPushMatrix(); glLoadIdentity(); gluOrtho2D(0, 800, 0, 600);
     glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
+    glPushMatrix(); glLoadIdentity();
 
-    DeseneazaFundalTextura(zoomFactor, screenX, screenY);
+    // Calculam pozitia pe ecran folosind variabila de sus
+    float lupaEcranX = 400.0f + (x * factorConversie);
+    float lupaEcranY = 300.0f + (y * factorConversie);
+
+    glTranslatef(lupaEcranX, lupaEcranY, 0);
+    glScalef(zoomFactor, zoomFactor, 1.0f); // Zoom variabil de sus
+    glTranslatef(-lupaEcranX, -lupaEcranY, 0);
+
+    DeseneazaFundalPentruZoom();
 
     glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
+    glMatrixMode(GL_PROJECTION); glPopMatrix();
     glMatrixMode(GL_MODELVIEW);
+    glDisable(GL_STENCIL_TEST);
 
-    // --- PASUL 4: DESENARE LUPA ---
+    // --- PASUL 4: OBIECT LUPA 3D ---
     glEnable(GL_DEPTH_TEST);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION); glLoadIdentity();
     gluPerspective(45.0, 800.0 / 600.0, 1.0, 1000.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW); glLoadIdentity();
     glTranslatef(x, y, z);
 
     // MANER
@@ -194,7 +157,6 @@ void CALLBACK display(void) {
     glBindTexture(GL_TEXTURE_2D, texturi[0]);
     gluQuadricTexture(manerLupa, GL_TRUE);
     glPushMatrix();
-    glColor3f(1.0f, 1.0f, 1.0f);
     glTranslatef(0.0, -35.0, 0.0);
     glRotatef(90.0, 1.0, 0.0, 0.0);
     gluCylinder(manerLupa, 5, 5, 50, 20, 20);
@@ -207,46 +169,35 @@ void CALLBACK display(void) {
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, texturi[1]);
     gluQuadricTexture(ramaLupa, GL_TRUE);
-    glPushMatrix();
-    glColor3f(1.0f, 1.0f, 1.0f);
-    gluDisk(ramaLupa, 35, 40, 40, 1);
-    glPopMatrix();
+    gluDisk(ramaLupa, 35, 40, 45, 1);
     glDisable(GL_TEXTURE_2D);
     gluDeleteQuadric(ramaLupa);
 
     // LENTILA
     GLUquadricObj* lentilaLupa = gluNewQuadric();
-    glPushMatrix();
     glColor4f(0.5f, 0.7f, 1.0f, 0.2f);
-    gluDisk(lentilaLupa, 0, 35, 40, 1);
-    glPopMatrix();
+    gluDisk(lentilaLupa, 0, 35, 45, 1);
     gluDeleteQuadric(lentilaLupa);
 
     auxSwapBuffers();
 }
 
-// Funcții de control - DOAR deplasări
-void CALLBACK MutaStanga(void) { x -= 10; }
-void CALLBACK MutaDreapta(void) { x += 10; }
-void CALLBACK MutaSus(void) { y += 10; }
-void CALLBACK MutaJos(void) { y -= 10; }
+void CALLBACK MutaStanga(void) { x -= 5; }
+void CALLBACK MutaDreapta(void) { x += 5; }
+void CALLBACK MutaSus(void) { y += 5; }
+void CALLBACK MutaJos(void) { y -= 5; }
 
 void CALLBACK myReshape(GLsizei w, GLsizei h) {
     if (h == 0) h = 1;
     glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0, (GLfloat)w / (GLfloat)h, 1.0, 1000.0);
-    glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv) {
     auxInitDisplayMode(AUX_DOUBLE | AUX_RGBA | AUX_DEPTH | AUX_STENCIL);
     auxInitPosition(100, 100, 800, 600);
-    auxInitWindow("Lupa Optica - Proiect");
+    auxInitWindow("Proiect Lupa ");
     myinit();
 
-    // Control DOAR cu săgeți pentru deplasare
     auxKeyFunc(AUX_LEFT, MutaStanga);
     auxKeyFunc(AUX_RIGHT, MutaDreapta);
     auxKeyFunc(AUX_UP, MutaSus);
